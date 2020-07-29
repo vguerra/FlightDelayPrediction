5c088119ff6f40ed423c3813b7c4382c9e579ae8
sleroy <s.leroy@criteo.com>
2020-07-29 19:02:00 +0200

    initial commit

---
+++ Flight.scala
@@ -0,0 +1,24 @@
+// spark-submit --master yarn --conf spark.driver.cores=12 --class "FlightProject" target/scala-2.11/flightproject_2.11-1.0.jar
+
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification._
+import org.apache.spark.ml.feature._
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.rdd.RDD
+
+object FlightProject {
+
+  def main(args: Array[String]) {
+    val conf: SparkConf = new SparkConf().setAppName("FlightProject")
+    implicit val sc: SparkContext = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    implicit val spark: SparkSession = SparkSession.builder.config(conf).getOrCreate()
+    import spark.implicits._
+
+    spark.stop()
+  }
+}
