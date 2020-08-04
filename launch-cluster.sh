510b53bfb829e91e9d0aa6af8ed14473e84c4764
Victor Guerra <vguerra@gmail.com>
2020-08-04 23:52:25 +0200

    12 vcores for executors

--- launch-cluster.sh
+++ launch-cluster.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 spark-submit --verbose --master yarn \
     --conf spark.driver.cores=12 \
+    --conf spark.executor.cores=12 \
     --num-executors 100 \
     --executor-memory 16g \
     --class FlightProject "${DIR}/target/scala-2.11/flightproject_2.11-1.0.jar" \
